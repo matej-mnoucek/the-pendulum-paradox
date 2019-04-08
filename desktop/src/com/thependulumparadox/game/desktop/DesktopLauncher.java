5b40a1c05f62cf04d17c5476b97d9bfabab532f4
Matěj Mňouček <matej.mnoucek@gmail.com>
2019-04-08 11:20:07 +0200

    Desktop launcher fix

--- desktop/src/com/thependulumparadox/game/desktop/DesktopLauncher.java
+++ desktop/src/com/thependulumparadox/game/desktop/DesktopLauncher.java
@@ -3,7 +3,6 @@ package com.thependulumparadox.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.thependulumparadox.game.PendulumParadoxGame;
-import com.thependulumparadox.presenter.GamePresenter;
 
 public class DesktopLauncher
 {
@@ -12,6 +11,6 @@ public class DesktopLauncher
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1000;
 		config.height = 600;
-		new LwjglApplication(new PendulumParadoxGame(), config);
+		new LwjglApplication(new PendulumParadoxGame(null), config);
 	}
 }
